c18c09729333185f693ad106c70e02b100f61b1c
Patrick Perry <patperry@gmail.com>
2019-03-29 16:33:14 -0500

    add another test

--- tests/schema.lua
+++ tests/schema.lua
@@ -1,30 +1,48 @@
 
-test = {
-    source = [[
-        {"a": 1, "b": 7.2}
-    ]],
-    schema = '{"a": "Int", "b": "Real"}'
+tests = {
+    {
+        source = [[
+            {"a": 1, "b": 7.2}
+        ]],
+        schema = '{"a": "Int", "b": "Real"}'
+    },
+    {
+        source = [[
+            {"a": 1, "b": 7.2}
+            {"a": 1.0, "b": 7.2}
+        ]],
+        schema = '{"a": "Real", "b": "Real"}'
+    },
 }
 
 function get_schema(source)
-    filename = os.tmpname()
+    local filename = os.tmpname()
 
-    file = assert(io.open(filename, "w"))
+    local file = assert(io.open(filename, "w"))
     assert(file:write(source))
     file:close()
 
-    stream = io.popen("./schema " .. filename)
-    output = stream:read("a")
+    local stream = io.popen("./schema " .. filename)
+    local output = stream:read("a")
     stream:close()
 
     os.remove(filename)
     return output
 end
 
-schema = get_schema(test.source)
-if schema ~= test.schema .. "\n" then
-    print("source: " .. test.source)
-    print("expected: " .. test.schema)
-    print("actual:   " .. schema)
+fail_count = 0
+
+for _, test in ipairs(tests) do
+    schema = get_schema(test.source)
+    if schema ~= test.schema .. "\n" then
+        print("source:\n" .. test.source)
+        print("expected: " .. test.schema)
+        print("actual:   " .. schema)
+        fail_count = fail_count + 1
+    end
+end
+
+if fail_count > 0 then
+    print(fail_count .. " tests failed")
     os.exit(false)
 end

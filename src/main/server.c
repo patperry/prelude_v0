cec4c8860596f0763a6bdb7920a9205e83211ac5
Patrick Perry <patperry@gmail.com>
2019-07-16 20:25:22 -0400

    localhost https doc link

--- src/main/server.c
+++ src/main/server.c
@@ -3,6 +3,7 @@
 
 #include "prelude.h"
 
+// https://www.freecodecamp.org/news/how-to-get-https-working-on-your-local-development-environment-in-5-minutes-7af615770eec/
 
 int main(int argc, const char **argv)
 {
